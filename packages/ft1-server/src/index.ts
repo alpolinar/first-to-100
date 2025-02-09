b1330465f6956a4cbf9de84920866477ef2d1d2d
Al Polinar <alpolinar@gmail.com>
2025-02-08 23:14:35 -0700

    clean up

--- packages/ft1-server/src/index.ts
+++ packages/ft1-server/src/index.ts
@@ -1,13 +1,13 @@
 import { cors } from "@elysiajs/cors";
 import { swagger } from "@elysiajs/swagger";
 import { Elysia } from "elysia";
-import gameRoute from "./ws";
+import ws from "./ws";
 import { env } from "./env";
 
 const app = new Elysia()
     .use(cors())
     .use(swagger({ path: "/docs" }))
-    .group("/sub", (app) => app.use(gameRoute))
+    .group("/sub", (app) => app.use(ws))
     .listen(env.PORT);
 
 console.log(
